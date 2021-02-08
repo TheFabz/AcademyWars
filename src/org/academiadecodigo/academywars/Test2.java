e0ea5e0c49cca40d53e2eb1867dd4691fb25dd83
joaolucianno <joaolucianno@gmail.com>
2021-02-08 21:17:51 +0000

    test enemy

--- src/org/academiadecodigo/academywars/Test2.java
+++ src/org/academiadecodigo/academywars/Test2.java
@@ -1,22 +1,32 @@
 package org.academiadecodigo.academywars;
+import org.academiadecodigo.academywars.chars.enemies.Asteroids;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Test2 {
     public static void main(String[] args) {
-        new Timer().scheduleAtFixedRate(new TimerTask(){
-            @Override
-            public void run(){
-                System.out.println("tag" + "A Kiss every 5 seconds");
-            }
-        },0,5000);
+        Rectangle field = new Rectangle(10, 10 , 1280, 720);
+        field.fill();
+        field.setColor(Color.WHITE);
+//        new Timer().scheduleAtFixedRate(new TimerTask(){
+//            @Override
+//            public void run(){
+//                System.out.println("tag" + "A Kiss every 5 seconds");
+//            }
+//        },0,5000);
+//
+//        new Timer().scheduleAtFixedRate(new TimerTask(){
+//            @Override
+//            public void run(){
+//                System.out.println("tag" + "A Kiss every 1 seconds");
+//            }
+//        },0,1000);
 
-        new Timer().scheduleAtFixedRate(new TimerTask(){
-            @Override
-            public void run(){
-                System.out.println("tag" + "A Kiss every 1 seconds");
-            }
-        },0,1000);
+        Asteroids asteroids = new Asteroids();
+        asteroids.avatar();
 
 
     }
--- src/org/academiadecodigo/academywars/chars/enemies/Asteroids.java
+++ src/org/academiadecodigo/academywars/chars/enemies/Asteroids.java
@@ -1,15 +1,99 @@
 package org.academiadecodigo.academywars.chars.enemies;
 
 import org.academiadecodigo.academywars.chars.Avatar;
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
 
-public class Asteroids extends Enemy{
+
+public class Asteroids /*extends Enemy*/{
     //Constructor
-    public Asteroids(int health, int maxDamage, Avatar avatar){
-        super(health, maxDamage, avatar);
+    public Asteroids(/*int health, int maxDamage, Avatar avatar*/){
+        //super(health, maxDamage, avatar);
     }
 
     //Custom Methods
     public void hit(int damage){ //Asteroids can't attack, BTW hit must be Override
+
+    }
+
+    public void avatar(){
+        Rectangle rec = new Rectangle(600, 300, 10, 10);
+        rec.fill();
+        rec.setColor(new Color(57, 49, 75));
+        Rectangle rec1 = new Rectangle(600, 310, 10, 10);
+        rec1.fill();
+        rec1.setColor(new Color(57, 49, 75));
+        Rectangle rec2 = new Rectangle(600, 320, 10, 10);
+        rec2.fill();
+        rec2.setColor(new Color(57, 49, 75));
+        Rectangle rec3 = new Rectangle(600, 330, 10, 10);
+        rec3.fill();
+        rec3.setColor(new Color(57, 49, 75));
+        Rectangle rec4 = new Rectangle(600, 340, 10, 10);
+        rec4.fill();
+        rec4.setColor(new Color(57, 49, 75));
+        Rectangle rec5 = new Rectangle(600, 350, 10, 10);
+        rec5.fill();
+        rec5.setColor(new Color(57, 49, 75));
+        Rectangle rec6 = new Rectangle(610, 350, 10, 10);
+        rec6.fill();
+        rec6.setColor(new Color(57, 49, 75));
+        Rectangle rec7 = new Rectangle(610, 360, 10, 10);
+        rec7.fill();
+        rec7.setColor(new Color(57, 49, 75));
+        Rectangle rec8 = new Rectangle(610, 370, 10, 10);
+        rec8.fill();
+        rec8.setColor(new Color(57, 49, 75));
+        Rectangle rec9 = new Rectangle(620, 370, 10, 10);
+        rec9.fill();
+        rec9.setColor(new Color(57, 49, 75));
+        Rectangle rec10 = new Rectangle(630, 370, 10, 10);
+        rec10.fill();
+        rec10.setColor(new Color(57, 49, 75));
+        Rectangle rec11 = new Rectangle(630, 360, 10, 10);
+        rec11.fill();
+        rec11.setColor(new Color(57, 49, 75));
+        Rectangle rec12 = new Rectangle(640, 360, 10, 10);
+        rec12.fill();
+        rec12.setColor(new Color(57, 49, 75));
+        Rectangle rec13 = new Rectangle(650, 360, 10, 10);
+        rec13.fill();
+        rec13.setColor(new Color(57, 49, 75));
+        Rectangle rec14 = new Rectangle(660, 360, 10, 10);
+        rec14.fill();
+        rec14.setColor(new Color(57, 49, 75));
+        Rectangle rec15 = new Rectangle(660, 370, 10, 10);
+        rec15.fill();
+        rec15.setColor(new Color(57, 49, 75));
+        Rectangle rec16 = new Rectangle(670, 370, 10, 10);
+        rec16.fill();
+        rec16.setColor(new Color(57, 49, 75));
+        Rectangle rec17 = new Rectangle(680, 370, 10, 10);
+        rec17.fill();
+        rec17.setColor(new Color(57, 49, 75));
+        Rectangle rec18 = new Rectangle(680, 360, 10, 10);
+        rec18.fill();
+        rec18.setColor(new Color(57, 49, 75));
+        Rectangle rec19 = new Rectangle(690, 350, 10, 10);
+        rec19.fill();
+        rec19.setColor(new Color(57, 49, 75));
+        Rectangle rec20 = new Rectangle(690, 340, 10, 10);
+        rec20.fill();
+        rec20.setColor(new Color(57, 49, 75));
+        Rectangle rec21 = new Rectangle(690, 330, 10, 10);
+        rec21.fill();
+        rec21.setColor(new Color(57, 49, 75));
+        Rectangle rec22 = new Rectangle(690, 320, 10, 10);
+        rec22.fill();
+        rec22.setColor(new Color(57, 49, 75));
+        Rectangle rec23 = new Rectangle(690, 310, 10, 10);
+        rec23.fill();
+        rec23.setColor(new Color(57, 49, 75));
+        Rectangle rec24 = new Rectangle(690, 300, 10, 10);
+        rec24.fill();
+        rec24.setColor(new Color(57, 49, 75));
+
+
 
     }
 }
